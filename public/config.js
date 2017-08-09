d5d1fb849c1a980989041d04a9365290cee96278
Alexander <alexxandreas@gmail.com>
2017-08-09 08:50:52 +0000

    собрал в public

---
+++ public/config.js
@@ -0,0 +1,8 @@
+var CKEDITOR_BASEPATH = '/static/ckeditor/';
+var appConfig = {
+
+    // apiUrl: 'http://sportsensus.ru:8080/api',
+    apiUrl: 'http://sportsensus.ru/api',
+    imageUploadUrl: 'http://sportsensus.ru/api/ckupload'
+    //proxyURL: 'https://cors-anywhere.herokuapp.com/',
+}; 
